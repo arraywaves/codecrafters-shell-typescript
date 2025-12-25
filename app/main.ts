3ec8c4d91b3e6ea1c70201269c3b9dbfbd37b43b
Alex <tech@arraywaves.dev>
2025-12-25 08:58:36 +0000

    command not found

--- app/main.ts
+++ app/main.ts
@@ -7,5 +7,7 @@ const rl = createInterface({
 
 // TODO: Uncomment the code below to pass the first stage
 rl.question("$ ", (answer) => {
+	console.log(`${answer}: command not found`);
+
 	rl.close();
 });
