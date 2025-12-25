c41cc184a034b13bc92332420937ffef72de8afe
Alex <tech@arraywaves.dev>
2025-12-25 08:52:35 +0000

    print a prompt

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,11 @@
 import { createInterface } from "readline";
 
 const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
 // TODO: Uncomment the code below to pass the first stage
-// rl.question("$ ", (answer) => {
-//   rl.close();
-// });
+rl.question("$ ", (answer) => {
+	rl.close();
+});
